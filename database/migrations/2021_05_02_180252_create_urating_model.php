c9f0b9a86d5bde536483f7484d9c2baef1662ee8
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-03 12:31:20 +0200

    :sparkles: add migrations create urating table

---
+++ database/migrations/2021_05_02_180252_create_urating_model.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUratingModel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uratings', function (Blueprint $table) {
+            $table->id();
+            $table->float('score')->nullable();
+            $table->string('speed')->nullable();
+            $table->float('accuracy')->nullable();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uratings');
+    }
+}
