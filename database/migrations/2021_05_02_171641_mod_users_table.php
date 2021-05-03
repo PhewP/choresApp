19ec921f1d3b6d23bcb0d81d21dccd7045a2c64e
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-03 12:30:40 +0200

    :sparkles: add migration update user with more fields

---
+++ database/migrations/2021_05_02_171641_mod_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table){
+            $table->string('province');
+            $table->string('address');
+            $table->string('city');
+            $table->float('coins')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
