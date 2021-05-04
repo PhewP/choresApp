46dc304c7173e52cf4f090769b3e824c78c22902
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-04 13:28:09 +0200

    :sparkles: add category migration

---
+++ database/migrations/2021_05_04_082743_create_category_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorys', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::table('tasks', function(Blueprint $table){
+            $table->foreignId('category_id')->constrained('categorys');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorys');
+    }
+}
