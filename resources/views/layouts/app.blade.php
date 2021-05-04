d2d3d6e93f078d34462ac407210973afae04f7ae
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-04 18:33:00 +0200

    :sparkles: add navbar to app base

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,8 +22,9 @@
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
+        @if(Auth::check())
+            @livewire('navbar') 
+        @endif
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
