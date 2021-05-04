3f8edd9f05db3a55f81a77afcc685b5d3742cfcf
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-04 13:28:22 +0200

    :sparkles: add tratings migration

---
+++ database/migrations/2021_05_04_085529_create_tratings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTratingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tratings', function (Blueprint $table) {
+            $table->id();
+            $table->float('score')->nullable();
+            $table->string('comment')->nullable();
+            $table->float('performance')->nullable();
+            $table->string('speed')->nullable();
+            $table->timestamps();
+            $table->foreignId('task_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tratings');
+    }
+}
