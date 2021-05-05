59c96640c2c9fb00b78ac9efe92c31aec4877244
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-05 14:03:21 +0200

    :recycle: change base to structure content

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,6 +1,15 @@
 <x-app-layout>
         @auth
-        @yield('content') 
+        <div class="container mt-4 mb-5">
+        <div class="d-flex justify-content-center row">
+            <div class="col-md-8">
+                <div class="feed p-2" id="feed-content">
+                        @yield('content') 
+                    <div id="posts-content"></div>
+                </div>
+            </div>
+        </div>
+        </div>
         @else 
         @yield('welcome')  
         @endauth
