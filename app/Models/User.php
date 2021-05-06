6de800172dfc3f6872122a30c58cdfc60cf2418d
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-06 10:57:43 +0200

    :recycle: change User model, add coins as fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','province', 'city', 'address'
+        'name', 'email', 'password','province', 'city', 'address','coins'
     ];
 
     /**
