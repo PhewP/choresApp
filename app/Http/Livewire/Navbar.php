befdd88159a4170b1678496df9fa874de9961aa3
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-06 10:58:11 +0200

    :recycle: add coins to navbar component

--- app/Http/Livewire/Navbar.php
+++ app/Http/Livewire/Navbar.php
@@ -1,13 +1,18 @@
 <?php
 
 namespace App\Http\Livewire;
+use Illuminate\Support\Facades\Auth;
+
 
 use Livewire\Component;
 
 class Navbar extends Component
 {
+    public $coins;
+
     public function render()
     {
+        $this->coins = auth()->user()->coins;
         return view('livewire.navbar');
     }
 }
