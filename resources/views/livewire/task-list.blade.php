0fff3b5d77dcc62ccccee1ec929fd77b7c089ba1
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-06 17:39:09 +0200

    :recycle: change logic to display message if not province task

--- resources/views/livewire/task-list.blade.php
+++ resources/views/livewire/task-list.blade.php
@@ -1,11 +1,17 @@
 <div style="margin-bottom: 30px;">
     <div class="d-flex flex-row justify-content-between align-items-center p-2 bg-white border">
         <div class="feed-text px-2">
+            @if( isset($taskList) && !$taskList->isEmpty())
             @foreach($taskList as $task)
-                <div>
-                    <a href="{{ route('task_detail', ['task'=>$task->id]) }}" class="underline">{{$task->title}}</a>
-                </div>
+            <div>
+                <a href="{{ route('task_detail', ['task'=>$task->id]) }}" class="underline">{{$task->title}}</a>
+            </div>
             @endforeach
+            @else
+            <div>
+                Todavía no hay tareas creadas en su provincia! sea el primero!
+            </div>
+            @endif
         </div>
     </div>
 </div>
