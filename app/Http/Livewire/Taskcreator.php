7e6247a67d850b7c23a12147afe7a85e4a823cef
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-06 10:58:26 +0200

    :construction: add form to taskcreator

--- app/Http/Livewire/Taskcreator.php
+++ app/Http/Livewire/Taskcreator.php
@@ -1,19 +1,61 @@
 <?php
 
 namespace App\Http\Livewire;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Category;
+use App\Models\Task;
+use Illuminate\Validation\Rule;
+
 
 use Livewire\Component;
 
 class Taskcreator extends Component
 {
-    public $task='';
+    public $title;
+    public $reward;
+    public $description;
+    public $categoryName;
+    public $ini_date;
+    public $end_date;
+
+    public $categoryNames = [];
+
+
+    protected function rules() {
+        return [
+            'title' => ['required', 'min:10'],
+            'reward' => ['required', 'numeric', 'min:1', 'max:'.auth()->user()->coins],
+            'description' => ['required', 'min:20'], 
+            'categoryName' =>['required', Rule::in([$this->categoryName])],
+            'ini_date' => ['required', 'after:'.now()],
+            'end_date' => ['required', 'after:'.$this->ini_date]
+        ];
+    }
+
+    public function updated($propertyName) {
+        $this->validateOnly($propertyName);
+    }
+
     public function createTask() {
-        $this->task='creado';
-        echo('creado');
+        $this->validate();
+        $id = Auth::id();
+        $category = Category::where('name', $this->categoryName)->first();
+
+        if(isset($category)) {
+
+        }
+    }
+
+    public function mount() {
+        $allCategories = Category::get();
+        foreach($allCategories as $category) {
+            $this->categoryNames[] = $category->name;
+        }
     }
 
     public function render()
     {
+        // $task = Task::where('id', id);
         return view('livewire.taskcreator');
     }
 }
--- resources/views/livewire/taskcreator.blade.php
+++ resources/views/livewire/taskcreator.blade.php
@@ -4,20 +4,73 @@
         <div class="feed-text px-2">
             <h6 class="mt-2">Crear tarea</h6>
         </div>
-      <i id="plusButton" class="fa fa-plus" role="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+      <i id="plusButton" class="fa fa-plus" role="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded:="true" aria-controls="collapseExample">
       </i>
     </div>
-    <div class="collapse" id="collapseExample">
+    <div class="collapse.show" id="collapseExample">
     
       <div>
           <div class="card card-body">
-            Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
-        
-          <div class="mt-3 mr-3">
-              <x-jet-button wire:click="createTask">Crear</x-jet-button>
-          </div>
+
+          <form wire:submit.prevent="createTask">
+
+            <div class="col-span-6 sm:col-span-4">
+              <x-jet-label for="title" value="{{ __('Título') }}" />
+              <x-jet-input id="title" type="text" class="mt-1 block w-full" wire:model="title"/>
+              <x-jet-input-error for="title" class="mt-2" />
+            </div>
+
+            <div class="col-span-6 sm:col-span-4">
+              <x-jet-label for="description" value="{{ __('Descripción') }}" />
+              <x-jet-input id="description" type="text" class="mt-1 block w-full" wire:model="description"/>
+              <x-jet-input-error for="description" class="mt-2" />
+            </div>
+          
+            <div class="form-group">
+                <x-jet-label for="categoryName" value="{{ __('Categoría') }}"/>
+                <select required class="form-control" wire:model="categoryName">
+                    <option disabled selected>Selecciona una opción</option>
+                    @foreach($categoryNames as $category)
+                      <option>{{$category}}</option>
+                    @endforeach
+                </select>
+            </div>
+
+          
+            <div class="field">
+              <x-jet-label for="ini_date" value="{{ __('Fecha inicio') }}"/>
+              <div class="control">
+                  <x-jet-input id="ini_date" wire:model="ini_date" class="mt-1 block w-full" type="date" min={{now()}}/>
+                  <x-jet-input-error for="ini_date" class="mt-2" />
+              </div>
+            </div>
+
+            <div class="field">
+              <x-jet-label for="end_date" value="{{ __('Fecha limite') }}"/>
+              <div class="control">
+                  <x-jet-input id="end_date" wire:model="end_date" class="mt-1 block w-full" type="date" min={{now()}}/>
+                  <x-jet-input-error for="end_date" class="mt-2" />
+              </div>
+            </div>
+            
+            <div class="col-span-6 sm:col-span-4">
+              <x-jet-label for="reward" value="{{ __('Recompensa') }}" />
+              <x-jet-input id="reward" type="number" class="mt-1 block w-full" wire:model="reward"/>
+              <x-jet-input-error for="reward" class="mt-2" />
+            </div>
+
+            <div class="mt-3 mr-3">
+                <x-jet-button type="submit">Crear</x-jet-button>
+            </div>
+            <div class="mt-3 mr-3">
+                <x-jet-button type="submit">Limpiar</x-jet-button>
+            </div>
+
+          </form>
           </div>
       </div>
     </div>
+
+
 </div>
 
