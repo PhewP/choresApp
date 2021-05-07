4ed017b305bcced0ddc8528db654cf1b2e84d320
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-07 09:39:15 +0200

    :construction: separete task in list task

--- resources/views/livewire/task-list.blade.php
+++ resources/views/livewire/task-list.blade.php
@@ -1,17 +1,17 @@
-<div style="margin-bottom: 30px;">
-    <div class="d-flex flex-row justify-content-between align-items-center p-2 bg-white border">
-        <div class="feed-text px-2">
-            @if( isset($taskList) && !$taskList->isEmpty())
-            @foreach($taskList as $task)
-            <div>
-                <a href="{{ route('task_detail', ['task'=>$task->id]) }}" class="underline">{{$task->title}}</a>
+    @if( isset($taskList) && !$taskList->isEmpty())
+    @foreach($taskList as $task)
+    <div style="margin-bottom: 30px;">
+        <div class="d-flex flex-row justify-content-between align-items-center p-2 bg-white border">
+            <div class="feed-text px-2">
+                <div>
+                    <a href="{{ route('task_detail', ['task'=>$task->id]) }}" class="underline">{{$task->title}}</a>
+                </div>
             </div>
-            @endforeach
-            @else
-            <div>
-                Todavía no hay tareas creadas en su provincia! sea el primero!
-            </div>
-            @endif
         </div>
     </div>
-</div>
+    @endforeach
+    @else
+    <div>
+        Todavía no hay tareas creadas en su provincia! sea el primero!
+    </div>
+    @endif
