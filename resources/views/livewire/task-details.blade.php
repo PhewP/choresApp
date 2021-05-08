bbba2bc959a21675679a26e7d7073814e8f0dc67
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-08 13:45:24 +0200

    :sparkles: show message when accept task in task details

--- resources/views/livewire/task-details.blade.php
+++ resources/views/livewire/task-details.blade.php
@@ -1,99 +1,105 @@
 <div style="margin-bottom: 30px;">
-    <div class="d-flex flex-row justify-content-between align-items-center p-2 bg-white border">
-        <div class="feed-text px-2">
-            <div class="container">
-            <center>
-            <h3>{{$task->title}}</h3>
-            <b>Descripción:</b>
-                {{$task->description}}
-                <br>
-                <b>Disponible a partir de:</b>
-                {{$task->ini_date}}
-                <br>
-                <b>Disponible hasta:</b>
-                {{$task->end_date}}
-                <br>
-                <b>Recompensa:</b>
-                {{$task->reward}} coins
-                <br>
-                <b>Estado:</b>
-                {{$task->status}}
-                @if($task->creator_id == Auth::id())
-                    <div class="mt-3 mr-3">
-<div style="margin-bottom: 30px;">
-
-    <div>
-      <div class="card card-body">
-
-        <form wire:submit.prevent="modifyTask">
-
-          <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="title" value="{{ __('Título') }}" />
-            <x-jet-input id="title" type="text" class="mt-1 block w-full" wire:model="title"/>
-            <x-jet-input-error for="title" class="mt-2" />
-          </div>
-
-          <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="description" value="{{ __('Descripción') }}" />
-            <x-jet-input id="description" type="text" class="mt-1 block w-full" wire:model="description" />
-            <x-jet-input-error for="description" class="mt-2" />
-          </div>
-
-          <div class="form-group">
-            <x-jet-label for="categoryName" value="{{ __('Categoría') }}" />
-            <select required class="form-control" wire:model="categoryName">
-              <option selected hidden value="{{ $task->category }}"></option>
-              @foreach($categoryNames as $category)
-              <option>{{$category}}</option>
-              @endforeach
-            </select>
-            <x-jet-input-error for="categoryName" class="mt-2" />
-          </div>
-
-
-          <div class="field">
-            <x-jet-label for="ini_date" value="{{ __('Fecha inicio') }}" />
-            <div class="control">
-              <x-jet-input id="ini_date" wire:model="ini_date" class="mt-1 block w-full" type="date" min={{now()}} />
-              <x-jet-input-error for="ini_date" class="mt-2" />
-            </div>
-          </div>
-
-          <div class="field">
-            <x-jet-label for="end_date" value="{{ __('Fecha limite') }}" />
-            <div class="control">
-              <x-jet-input id="end_date" wire:model="end_date" class="mt-1 block w-full" type="date" min={{now()}} />
-              <x-jet-input-error for="end_date" class="mt-2" />
-            </div>
-          </div>
-
-          <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="reward" value="{{ __('Recompensa') }}" />
-            <x-jet-input id="reward" type="number" class="mt-1 block w-full" wire:model="reward" />
-            <x-jet-input-error for="reward" class="mt-2" />
-          </div>
-
+  <div class="d-flex flex-row justify-content-between align-items-center p-2 bg-white border">
+    <div class="feed-text px-2">
+      <div class="container">
+        <center>
+          <h3>{{$task->title}}</h3>
+          <b>Descripción:</b>
+          {{$task->description}}
+          <br>
+          <b>Disponible a partir de:</b>
+          {{$task->ini_date}}
+          <br>
+          <b>Disponible hasta:</b>
+          {{$task->end_date}}
+          <br>
+          <b>Recompensa:</b>
+          {{$task->reward}} coins
+          <br>
+          <b>Estado:</b>
+          {{$task->status}}
+          @if($task->creator_id == Auth::id())
           <div class="mt-3 mr-3">
-            <x-jet-button type="submit">Editar</x-jet-button>
-          </div>
-          <div class="mt-3 mr-3">
-            <x-jet-button type="submit">Limpiar</x-jet-button>
-          </div>
+            <div style="margin-bottom: 30px;">
 
-        </form>
-      </div>
-  </div>
-</div>
-                @else
-                <div class="mt-3 mr-3">
-                        <x-jet-button>
-                            Aceptar<!-- <a href="{{ route('task_detail', ['task'=>$task->id]) }}" class="underline">{{$task->title}}</a> -->
-                        </x-jet-button>
+              <div>
+                <div class="card card-body">
+
+                  <form wire:submit.prevent="modifyTask">
+
+                    <div class="col-span-6 sm:col-span-4">
+                      <x-jet-label for="title" value="{{ __('Título') }}" />
+                      <x-jet-input id="title" type="text" class="mt-1 block w-full" wire:model="title" />
+                      <x-jet-input-error for="title" class="mt-2" />
                     </div>
-                @endif
-            </center>
+
+                    <div class="col-span-6 sm:col-span-4">
+                      <x-jet-label for="description" value="{{ __('Descripción') }}" />
+                      <x-jet-input id="description" type="text" class="mt-1 block w-full" wire:model="description" />
+                      <x-jet-input-error for="description" class="mt-2" />
+                    </div>
+
+                    <div class="form-group">
+                      <x-jet-label for="categoryName" value="{{ __('Categoría') }}" />
+                      <select required class="form-control" wire:model="categoryName">
+                        <option selected hidden value="{{ $task->category }}"></option>
+                        @foreach($categoryNames as $category)
+                        <option>{{$category}}</option>
+                        @endforeach
+                      </select>
+                      <x-jet-input-error for="categoryName" class="mt-2" />
+                    </div>
+
+
+                    <div class="field">
+                      <x-jet-label for="ini_date" value="{{ __('Fecha inicio') }}" />
+                      <div class="control">
+                        <x-jet-input id="ini_date" wire:model="ini_date" class="mt-1 block w-full" type="date" min={{now()}} />
+                        <x-jet-input-error for="ini_date" class="mt-2" />
+                      </div>
+                    </div>
+
+                    <div class="field">
+                      <x-jet-label for="end_date" value="{{ __('Fecha limite') }}" />
+                      <div class="control">
+                        <x-jet-input id="end_date" wire:model="end_date" class="mt-1 block w-full" type="date" min={{now()}} />
+                        <x-jet-input-error for="end_date" class="mt-2" />
+                      </div>
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-4">
+                      <x-jet-label for="reward" value="{{ __('Recompensa') }}" />
+                      <x-jet-input id="reward" type="number" class="mt-1 block w-full" wire:model="reward" />
+                      <x-jet-input-error for="reward" class="mt-2" />
+                    </div>
+
+                    <div class="mt-3 mr-3">
+                      <x-jet-button type="submit">Editar</x-jet-button>
+                    </div>
+                    <div class="mt-3 mr-3">
+                      <x-jet-button type="submit">Limpiar</x-jet-button>
+                    </div>
+
+
+                  </form>
+                </div>
+              </div>
             </div>
-        </div>
+            @elseif(!$accepted)
+            <div class="mt-3 mr-3">
+              <x-jet-button wire:click.stop="acceptTask">
+                Aceptar
+                <!-- <a href="{{ route('task_detail', ['task'=>$task->id]) }}" class="underline">{{$task->title}}</a> -->
+              </x-jet-button>
+              @else
+              <div class="alert alert-success">
+                Tarea Aceptada
+              </div>
+            </div>
+            @endif
+        </center>
+      </div>
     </div>
-    
+  </div>
+
 </div>
