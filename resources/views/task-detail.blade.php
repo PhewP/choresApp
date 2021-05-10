af38a557e61d1545ad7dcf0d528ddd9db3fbe676
jeffresh <manuel.pallanunez@alum.uca.es>
2021-05-10 18:05:32 +0200

    :sparkles: add privacy when task is accepted

--- resources/views/task-detail.blade.php
+++ resources/views/task-detail.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.base')
 @section('content')
+@if(auth()->user()->id == $task->creator_id || auth()->user()->id == $task->performer_id)
 @livewire('task-details', ['task' => $task])
+@else
+<div>
+    <div class="flex-row align-items-center p-2 bg-white border">
+        <div class="feed-text px-2">
+            <div class="container">
+                <center>
+                    Tarea no disponible
+                </center>
+            </div>
+        </div>
+    </div>
+</div>
+@endif
 @endsection
